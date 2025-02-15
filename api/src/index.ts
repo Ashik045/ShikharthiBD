48b3cdea6093846bbac65cbeb0ee6730dc343cf7
Ashik045 <ashikurislam045@gmail.com>
2025-02-15 17:32:41 +0600

    first commit

---
+++ api/src/index.ts
@@ -0,0 +1,33 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express, { Request, Response } from "express";
+
+// Initialize Express app
+dotenv.config();
+const app = express();
+
+// Middleware.
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Simple route
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).json({ message: "Ok" });
+});
+
+// Error handling: 404
+app.use((req: Request, res: Response) => {
+  res.status(404).json({ error: "Requested URL not found!" });
+});
+
+// Default error handler
+app.use((err: Error, req: Request, res: Response, next: Function) => {
+  res.status(500).json({ error: "Internal Server Error!" });
+});
+
+// Start the server
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
---
+++ client/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1>shikharthibd</h1>
+      <p>updated</p>
+    </div>
+  );
+}
