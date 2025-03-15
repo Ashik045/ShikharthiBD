fc4703be046f79a9467a6b46bf0ce575412c9957
Ashik045 <ashikurislam045@gmail.com>
2025-03-15 11:18:49 +0600

    ssc & hsc page created

--- client/app/questions/ssc/page.tsx
+++ client/app/questions/ssc/page.tsx
@@ -1,5 +1,34 @@
+import Filter from "@/components/Filter/Filter";
+import ResultsTable from "@/components/ResultsTable/ResultsTable";
+import styles from "@/styles/questions.module.scss";
+
+const pdfUrl = "/pdfs/Ashikur_CV.pdf"; // No need to import
+
 const ssc = () => {
-  return <div>ssc</div>;
+  const results = [
+    {
+      id: 1,
+      name: "Math 2023 Question",
+      pdfUrl: pdfUrl,
+    },
+    {
+      id: 2,
+      name: "Physics 2022 Question",
+      pdfUrl: pdfUrl,
+    },
+  ];
+
+  return (
+    <div className={styles.page_container}>
+      {/* Pass filters to the Filter component */}
+      <div className={styles.filter_wrapper}>
+        <Filter ssc />
+      </div>
+
+      {/* show the results here */}
+      <ResultsTable results={results} />
+    </div>
+  );
 };
 
 export default ssc;
--- client/components/Filter/Filter.tsx
+++ client/components/Filter/Filter.tsx
@@ -67,6 +67,15 @@ const Filter = ({ hons, ssc, hsc, admission }: FilterParams) => {
               <option value="physics">GEOGRAPHY AND ENVIRONMENT </option>
               <option value="physics">MATHEMATICS </option>
             </select>
+            {/*  */}
+            <select value={year} onChange={(e) => setYear(e.target.value)}>
+              <option value="">Course Year</option>
+              <option value="2023">First year</option>
+              <option value="2023">Second year</option>
+              <option value="2023">Third year</option>
+              <option value="2023">Fourth year</option>
+            </select>
+            {/*  */}
             <select value={year} onChange={(e) => setYear(e.target.value)}>
               <option value="">Select Year</option>
               <option value="2023">2024</option>
@@ -83,25 +92,85 @@ const Filter = ({ hons, ssc, hsc, admission }: FilterParams) => {
           </>
         )}
 
-        {ssc ||
-          (hsc && (
-            <>
-              <select value={board} onChange={(e) => setBoard(e.target.value)}>
-                <option value="">Select Board</option>
-                <option value="dhaka">Dhaka</option>
-                <option value="rajshahi">Rajshahi</option>
-              </select>
+        {(ssc || hsc) && (
+          <>
+            <select
+              value={university}
+              onChange={(e) => setUniversity(e.target.value)}
+            >
+              <option value="">Select Group</option>
+              <option value="">Science</option>
+              <option value="">Humanities</option>
+            </select>
+            <select value={board} onChange={(e) => setBoard(e.target.value)}>
+              <option value="">Select Board</option>
+              <option value="dhaka">Dhaka</option>
+              <option value="rajshahi">Rajshahi</option>
+              <option value="comilla">Comilla</option>
+              <option value="barisal">Barisal</option>
+              <option value="sylhet">Sylhet</option>
+              <option value="dinajpur">Dinajpur</option>
+              <option value="chittagong">Chittagong</option>
+            </select>
 
-              <select
-                value={university}
-                onChange={(e) => setUniversity(e.target.value)}
-              >
-                <option value="">Select University</option>
-                <option value="Dhaka University">Dhaka University</option>
-                <option value="Rajshahi University">Rajshahi University</option>
-              </select>
-            </>
-          ))}
+            <select value={year} onChange={(e) => setYear(e.target.value)}>
+              <option value="">Select Year</option>
+              <option value="2023">2024</option>
+              <option value="2023">2023</option>
+              <option value="2022">2022</option>
+              <option value="2022">2021</option>
+              <option value="2022">2020</option>
+              <option value="2022">2019</option>
+              <option value="2022">2018</option>
+              <option value="2022">2017</option>
+              <option value="2022">2016</option>
+              <option value="2022">2015</option>
+            </select>
+          </>
+        )}
+
+        {admission && (
+          <>
+            <select
+              value={university}
+              onChange={(e) => setUniversity(e.target.value)}
+            >
+              <option value="">Select Group</option>
+              <option value="">Science</option>
+              <option value="">Humanities</option>
+            </select>
+
+            <select
+              value={university}
+              onChange={(e) => setUniversity(e.target.value)}
+            >
+              <option value="">Select University</option>
+              <option value="Dhaka University">Dhaka University</option>
+              <option value="Rajshahi University">Rajshahi University</option>
+              <option value="Jahangirnagar University">
+                Jahangirnagar University
+              </option>
+              <option value="Chittagong University">
+                Chittagong University
+              </option>
+              <option value="gst">GST</option>
+            </select>
+
+            <select value={year} onChange={(e) => setYear(e.target.value)}>
+              <option value="">Select Year</option>
+              <option value="2023">2024</option>
+              <option value="2023">2023</option>
+              <option value="2022">2022</option>
+              <option value="2022">2021</option>
+              <option value="2022">2020</option>
+              <option value="2022">2019</option>
+              <option value="2022">2018</option>
+              <option value="2022">2017</option>
+              <option value="2022">2016</option>
+              <option value="2022">2015</option>
+            </select>
+          </>
+        )}
       </div>
 
       <button className={styles.apply_filter} onClick={handleApply}>
