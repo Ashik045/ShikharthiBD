2f7148397e48580700ae06ef3f8b1ae123ad541a
Ashik045 <ashikurislam045@gmail.com>
2025-05-27 13:11:56 +0600

    updated

--- client/app/interview-questions/page.tsx
+++ client/app/interview-questions/page.tsx
@@ -1,3 +1,3 @@
 export default function interviewquestions() {
-  return <div style={{ paddingTop: "130px" }}>interviewquestions</div>;
+  return <div style={{ paddingTop: "130px" }}>interviewquestions page</div>;
 }
